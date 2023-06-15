e034988e771a4ab89cfbf96a3b5801e0e9ace502
Hitesh Moudgil <200378502@student.georgianc.on.ca>
2023-06-15 16:06:16 -0400

    made initial UI

---
+++ Dear Diary/Dear Diary/Registration/SignIn/SignInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignInViewController.swift
+//  Dear Diary
+//
+//  Created by Hitesh Moudgil on 2023-06-15.
+//  Copyright © 2023 Dear Diary. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    @IBOutlet weak var welcomeBackLabel: UILabel!
+    @IBOutlet weak var welcomeInfoLabel: UILabel!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var forgotPasswordButton: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signInWithGoogleButton: UIButton!
+    @IBOutlet weak var signUpTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Dear Diary/Dear Diary/Registration/SignIn/SignInViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  SignInViewModel.swift
+//  Dear Diary
+//
+//  Created by Hitesh Moudgil on 2023-06-15.
+//  Copyright © 2023 Dear Diary. All rights reserved.
+//
+
+import Foundation
